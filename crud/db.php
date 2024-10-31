657afd6ada42684eafe8b301ce3ad7ba1c13515c
andreannov <160991378+andreannov@users.noreply.github.com>
2024-11-01 00:25:48 +0700

    Add files via upload

---
+++ crud/db.php
@@ -0,0 +1,9 @@
+<?php
+$host = 'localhost';
+$dbusername = 'root';
+$dbpassword = '';
+$dbname = 'crud';
+$mysqli = new mysqli($host, $dbusername, $dbpassword, $dbname);
+if ($mysqli->connect_errno) {
+die('Database connection failure');
+}
